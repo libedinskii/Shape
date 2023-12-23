e5331d821dbae7ee27a4393072097c523cc28c63
libedinskii <124511733+libedinskii@users.noreply.github.com>
2023-12-23 19:04:52 +0900

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,12 @@
+public class Shape {
+    protected double area;
+    protected double perimeter;
+
+    public double getArea() {
+        return area;
+    }
+
+    public double getPerimeter() {
+        return perimeter;
+    }
+}
